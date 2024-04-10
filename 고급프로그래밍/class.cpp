40849e498d224a66db514f0a81209bb14bebf172
Simplexer <72856990+juho-creator@users.noreply.github.com>
2024-04-10 17:07:30 +0900

    Create class.cpp

---
+++ 고급프로그래밍/class.cpp
@@ -0,0 +1,41 @@
+/* Demonstration of class with invariant, rules for valid value defined at is_valid() */
+
+#include <iostream>
+
+using namespace std;
+
+class Date {
+private:
+	int year, month, day;
+	bool is_valid(int y, int m, int d);
+
+public:
+	Date(int y, int m, int d);
+	class Invalid {};
+};
+
+bool Date::is_valid(int y, int m, int d) {
+	if (y < 1 || m < 1 || m > 12 || d < 1 || d > 31)
+	{
+		return false;
+	}
+	return true;
+}
+
+Date::Date(int y, int m, int d)
+{
+	if (!is_valid(y, m, d)) {
+		throw Invalid();
+	}
+}
+
+
+int main() {
+	try {
+		Date birthday{ 2002, 123230, 8 };
+		cout << "Valid Date" << endl;
+	}
+	catch (Date::Invalid) {
+		cout << "Invalid Date" << endl;
+	}
+}
